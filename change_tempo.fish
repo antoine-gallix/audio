d3fc5febc64c3bf69d543e1778ac97a5e7df2c68
Antoine Gallix <antoine@antoine-gallix.me>
2022-02-09 02:01:44 +0100

    change tempo wrapper

--- change_tempo.fish
+++ change_tempo.fish
@@ -1,28 +1,23 @@
 #!/usr/bin/env fish
 
 # parsing
-argparse 'i/in=' 'o/out=' 'b/base-tempo=' 't/target-tempo=' 'g/go' -- $argv
-set INPUT_DIR (string trim --right -c '/' $_flag_in)
-set OUTPUT_DIR (string trim --right -c '/' $_flag_out)
-set FACTOR (math $_flag_target_tempo / $_flag_base_tempo)
+argparse 'g/go' -- $argv
+set input_file $argv[1]
+set output_file $argv[2]
+set input_tempo $argv[3]
+set output_tempo $argv[4]
 
+set factor (math $output_tempo / $input_tempo)
 
-# echo
-echo changing tempo from $_flag_base_tempo to $_flag_target_tempo
-echo input dir: $INPUT_DIR
-echo output dir: $OUTPUT_DIR
-if test -z "$_flag_go"
-    echo this is a test run. to execute the commands, use flag --go
-end
-echo
+echo changing tempo from $input_tempo bpm to $output_tempo bpm
 
+set command sox -V3 (string escape $input_file) (string escape $output_file) tempo $factor 120 30 30
+echo $command
 # process
-for infile in $INPUT_DIR/*
-    set outfile $OUTPUT_DIR/(basename $infile)
-    set command sox -V3 (string escape $infile) (string escape $outfile) tempo $FACTOR 120 30 30
-    echo $command
-    if test -n "$_flag_go"
-        eval $command
-    end
-
+if test -n "$_flag_go"
+    echo \n----------\n
+    eval $command
+    echo \n----------\n
+else
+    echo command was not executed. use flag --go to do so. 
 end
